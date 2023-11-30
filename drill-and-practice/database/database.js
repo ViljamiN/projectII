8530142f8ae8c42dc46ee8dd0eb5445ad399a03f
Viljami <viljami.narinen@aalto.fi>
2023-11-30 18:59:13 +0200

    changes to main

--- drill-and-practice/database/database.js
+++ drill-and-practice/database/database.js
@@ -2,18 +2,8 @@ import { postgres } from "../deps.js";
 
 let sql;
 
-if (Deno.env.get("PGUSER")) {
-    sql = postgres({
-        user: Deno.env.get("PGUSER"),
-        password: Deno.env.get("PGPASSWORD"),
-        database: Deno.env.get("PGDATABASE"),
-        host: Deno.env.get("PGHOST"),
-        port: parseInt(Deno.env.get("PGPORT")),
-    });
-}
-else {
-    console.log("test");
-    sql = postgres({});
-}
+//get the database connection parameters from the environment file "project.env"
+//if the environment file does not exist, use the default values
+
 
 export { sql };
