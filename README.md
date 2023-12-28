1f71608453cbd0b379b398758e0efdb333311afb
Viljami <viljami.narinen@aalto.fi>
2023-12-28 20:08:37 +0200

    updates

--- README.md
+++ README.md
@@ -6,17 +6,18 @@ As a user you can access topics created by admins by clicking the topics link.
 You can choose any topic, and create and delete questions and multiple choice answers for that topic.
 There is also a quiz tab where you can answer questions submitted by yourself or other users.
 On the main page you can see some application statistics.
+There is a navigation bar on the top of the page, where you can access the home, topics and quiz -pages.
 
 The application is **not running online** - it was specified in the assignment that having the application running online is currently not required.
 
 ## Running the application locally
 
-To run the application, open an integrated terminal in the root folder of the application and run the command `docker compose up --build`.
-You can stop the application with command `ctrl + C`, or by accessing it in Docker Desktop and clicking the stop button.
+To run the application, open an integrated terminal in the root folder of the application and run the command `docker compose up --build` (or `docker compose up` if you have already built the application).
+You can stop the application with command `ctrl + C`, or by accessing it via Docker Desktop and stopping it there.
 
 ## Running the tests
 
 The application comes with 10 end-to-end tests in the folder /e2e-playwright/tests.
 These tests are supposed to be run with an empty database, so remember to run the command `docker-compose rm -sf`before proceeding to run the tests.
 These tests can be run by opening an integrated terminal in the root folder and running the command `docker-compose run --entrypoint=npx e2e-playwright playwright test`.
-Note: you may need to run the command `docker-compose run --entrypoint=npm e2e-playwright install` before running the tests if you have not done so before.
+Note: you may need to run the command `docker-compose run --entrypoint=npm e2e-playwright install` before running the tests if you have not done so before (this was necessary at least for me when developing the tests).
--- e2e-playwright/tests/hello-world.spec.js
+++ e2e-playwright/tests/hello-world.spec.js
@@ -1,6 +1,22 @@
 const { test, expect } = require("@playwright/test");
 
-test("Server responds with correct heading and statistics", async ({ page }) => {
+const pagesToTest = [
+    { url: "http://localhost:7777", name: "Home" },
+    { url: "http://localhost:7777/auth/register", name: "Topics" },
+    { url: "http://localhost:7777/auth/login", name: "Quiz" },
+];
+
+const pagesToTestAfterLogin = [
+    { url: "http://localhost:7777/topics/", name: "Topics" },
+    { url: "http://localhost:7777/topics/1", name: "Topic 1" },
+    { url: "http://localhost:7777/topics/1/questions/1", name: "Question 1" },
+    { url: "http://localhost:7777/quiz/", name: "Quiz" },
+    { url: "http://localhost:7777/quiz/1/questions/1", name: "Quiz Question 1" },
+    { url: "http://localhost:7777/quiz/1/questions/1/correct", name: "Quiz Question 1 correct" },
+];
+
+// #1 Initial test to se that the application is running and the server responds
+test("Server responds with correct heading", async ({ page }) => {
     await page.goto("http://localhost:7777");
 
     // Check that the page has the correct heading
@@ -8,74 +24,185 @@ test("Server responds with correct heading and statistics", async ({ page }) => 
 
     // Check for statistics section
     await expect(page.locator("h2")).toHaveText("Application Statistics");
-
-    // Extract and check statistics using the new classname
-    console.log(await page.innerHTML("body"));
-    const topicsCount = page.locator("css.statistics");
-    // const questionsCount = await page.waitForSelector(".statistics li:nth-child(2)");
-    //const answersCount = await page.waitForSelector(".statistics li:nth-child(3)");
-
-    expect(topicsCount).toBe("Amount of topics: 1");
-    //expect(questionsCount.).toBe("Amount of questions: 0");
-    //expect(answersCount.).toBe("Amount of answers: 0");
 });
-/*
-test("Register Page response is correct", async ({ page }) => {
-    await page.goto("http://localhost:7777/auth/register");
+
+// #2
+test("Can open register page and register a new user", async ({ page }) => {
+    await page.goto("http://localhost:7777");
+    await page.locator(`a >> text='Register'`).click();
 
     //Check that the page has the correct heading
     await expect(page.locator("h1")).toHaveText("Register");
 
-    // Check for form elements
-    await expect(page.locator("form[method='POST'][action='/auth/register']")).toExist();
-    await expect(page.locator("form input[name='email']")).toExist();
-    await expect(page.locator("form input[name='password']")).toExist();
-    await expect(page.locator("form button[type='submit']")).toHaveText("Register");
+    // Register a new user
+    await page.locator("input[type=email]").type("teemu.teekkari@teekkari.fi");
+    await page.locator("input[type=password]").type("testPass");
+    await page.locator("button[type=submit]").click();
 
-    //Create a new user
-    await page.fill("form input[name='email']", "teemu.teekkari@teekkari.fi");
-    await page.fill("form input[name='password']", "salasana");
-    await page.click("form button[type='submit']");
+    //Check that redirection to the login page happened
+    await expect(page.locator("h1")).toHaveText("Login");
 });
 
-test("Login Page response is correct", async ({ page }) => {
+// #3
+test("Can open login page and login with a user", async ({ page }) => {
     await page.goto("http://localhost:7777/auth/login");
 
     //Check that the page has the correct heading
     await expect(page.locator("h1")).toHaveText("Login");
 
-    // Check for form elements
-    await expect(page.locator("form[method='POST'][action='/auth/login']")).toExist();
-    await expect(page.locator("form input[name='email']")).toExist();
-    await expect(page.locator("form input[name='password']")).toExist();
-    await expect(page.locator("form button[type='submit']")).toHaveText("Login");
-
-    //Login with a user
-    await page.fill("form input[name='email']", "teemu.teekkari@teekkari.fi");
-    await page.fill("form input[name='password']", "salasana");
-    await page.click("form button[type='submit']");
+    // Login with a user
+    await page.locator("input[type=email]").type("teemu.teekkari@teekkari.fi");
+    await page.locator("input[type=password]").type("testPass");
+    await page.locator("button[type=submit]").click();
 
     //Check that redirection to the topics page happened
     await expect(page.locator("h1")).toHaveText("Topics");
 });
 
-test("Topics Page 1 response is correct", async ({ page }) => {
-    await page.goto("http://localhost:7777/topics/1");
+// #4
+test("Topics response is correct when logged as a user", async ({ page }) => {
+    await page.goto("http://localhost:7777/topics/");
+
+    // Login with a user
+    await page.locator("input[type=email]").type("teemu.teekkari@teekkari.fi");
+    await page.locator("input[type=password]").type("testPass");
+    await page.locator("button[type=submit]").click();
 
     //Check that the page has the correct heading
     await expect(page.locator("h1")).toHaveText("Topics");
 
-    // Check for form elements
-    await expect(page.locator("form[method='POST'][action='/topics']")).toExist();
-    await expect(page.locator("form input[name='name']")).toExist();
-    await expect(page.locator("form button[type='submit']")).toHaveText("Add");
-
-    //Create a new topic
-    await page.fill("form input[name='name']", "Test topic");
-    await page.click("form button[type='submit']");
-
-    //Check that the topic was created
-    await expect(page.locator("a[role='button']")).toHaveText("Test topic");
-    
+    // Check that topic 1 exists on the page
+    await expect(page.locator("li")).toContainText(["Finnish language"]);
 });
-*/
+
+// #5
+test("Topics 1 response is correct when logged as a user", async ({ page }) => {
+    await page.goto("http://localhost:7777/topics/");
+
+    // Login with a user
+    await page.locator("input[type=email]").type("teemu.teekkari@teekkari.fi");
+    await page.locator("input[type=password]").type("testPass");
+    await page.locator("button[type=submit]").click();
+
+    //Check that the page has the correct heading
+    await expect(page.locator("h1")).toHaveText("Topics");
+
+    // Click on topic 1
+    await page.locator(`li >> text='Finnish language'`).click();
+
+    //check that the page has all the correct elements
+    await expect(page.locator("h1")).toHaveText("Finnish language");
+    await expect(page.locator("h2")).toHaveText("Write a question for this topic!");
+    await expect(page.locator("textarea")).toHaveText("");
+    await expect(page.locator("button[type=submit]")).toHaveText("Add");
+});
+
+// #6
+test("Can view quiz page when logged as a user", async ({ page }) => {
+    await page.goto("http://localhost:7777/auth/login/");
+
+    // Login with a user
+    await page.locator("input[type=email]").type("teemu.teekkari@teekkari.fi");
+    await page.locator("input[type=password]").type("testPass");
+    await page.locator("button[type=submit]").click();
+
+    await page.goto("http://localhost:7777/quiz/");
+
+    //Check that the page has the correct heading
+    await expect(page.locator("h1")).toHaveText("Quiz topics");
+
+    // Check that topic 1 exists on the page
+    await expect(page.locator("li")).toContainText(["Finnish language"]);
+});
+
+// #7
+test("Can view quiz for topic 1 when logged as a user", async ({ page }) => {
+    await page.goto("http://localhost:7777/auth/login/");
+
+    // Login with a user
+    await page.locator("input[type=email]").type("teemu.teekkari@teekkari.fi");
+    await page.locator("input[type=password]").type("testPass");
+    await page.locator("button[type=submit]").click();
+
+    await page.goto("http://localhost:7777/quiz/");
+
+    //Check that the page has the correct heading
+    await expect(page.locator("h1")).toHaveText("Quiz topics");
+
+    // Click on topic 1
+    await page.locator(`li >> text='Finnish language'`).click();
+
+    //check that the page has no questions
+    await expect(page.locator("h2")).toHaveText("This topic does not have any questions yet.");
+});
+
+// #8
+test("Can add a question to topic as a user", async ({ page }) => {
+    await page.goto("http://localhost:7777/auth/login/");
+
+    // Login with a user
+    await page.locator("input[type=email]").type("teemu.teekkari@teekkari.fi");
+    await page.locator("input[type=password]").type("testPass");
+    await page.locator("button[type=submit]").click();
+
+    await page.goto("http://localhost:7777/topics/");
+
+    // Click on topic 1
+    await page.locator(`li >> text='Finnish language'`).click();
+
+    // Add a question
+    await page.locator("textarea").type("test question");
+    await page.locator("button[type=submit]").click();
+
+    // check that the question was added
+    await expect(page.locator("div")).toContainText(["test question"]);
+});
+
+// #9
+test("Can add an answer option to a question as a user", async ({ page }) => {
+    await page.goto("http://localhost:7777/auth/login/");
+
+    // Login with a user
+    await page.locator("input[type=email]").type("teemu.teekkari@teekkari.fi");
+    await page.locator("input[type=password]").type("testPass");
+    await page.locator("button[type=submit]").click();
+
+    await page.goto("http://localhost:7777/topics/1");
+
+    // Click on the question
+    await page.locator(`div >> text='test question'`).click();
+
+    // check that the question is shown
+    await expect(page.locator("h1")).toHaveText("test question");
+    await expect(page.locator("h2")).toHaveText("Please enter the answer options for the question.");
+
+    // Add an answer option
+    await page.locator("textarea").type("test answer option");
+    //check iscorrect checkbox
+    await page.locator("input[type=checkbox]").check();
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    //check that the answer option was added
+    await expect(page.locator("div")).toContainText(["test answer option\nCorrectness: true"]);
+});
+
+// #10
+test(`Navigation bar exists on all pages when not logged in`, async ({ page }) => {
+    // Check that the navigation bar exists on all pages when not logged in
+    for (const pageToTest of pagesToTest) {
+        await page.goto(pageToTest.url);
+        await expect(page.locator("a.navlink")).toHaveText(["Home", "Topics", "Quiz"]);
+    }
+
+    // Login with a user
+    await page.goto("http://localhost:7777/auth/login");
+    await page.locator("input[type=email]").type("teemu.teekkari@teekkari.fi");
+    await page.locator("input[type=password]").type("testPass");
+    await page.locator("button[type=submit]").click();
+
+    // Check that the navigation bar exists on all pages when logged in
+    for (const pageToTest of pagesToTestAfterLogin) {
+        await page.goto(pageToTest.url);
+        await expect(page.locator("a.navlink")).toHaveText(["Home", "Topics", "Quiz"]);
+    }
+});
